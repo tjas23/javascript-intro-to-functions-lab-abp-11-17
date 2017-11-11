6cb46bcc0d752622312739a4deb3d7ab61d3ebab
Thomas Jascur <tjascur@yahoo.com>
2017-11-11 05:47:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,8 @@ function sayHiToGrandma(string) {
     return "I can't hear you!"
   }
 }
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return "YES INDEED!"
+  }
+}
